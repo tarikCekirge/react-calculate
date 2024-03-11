28f45705a15b6c292a00248f83a57a222d18af1c
Tarik Cekirge <tcekirge@gmail.com>
2024-03-11 21:24:20 +0300

    modified

--- src/component/CalculatorUseReducer.jsx
+++ src/component/CalculatorUseReducer.jsx
@@ -1,26 +1,6 @@
 import { useReducer } from "react";
 import "./Calculator.css";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SET_NUMBER1":
-      return { ...state, number1: action.payload };
-    case "SET_NUMBER2":
-      return { ...state, number2: action.payload };
-    case "SET_RESULT":
-      return { ...state, result: action.payload };
-    case "CLEAR":
-      return { ...state, number1: initialState.number1, number2: initialState.number2 };
-    default:
-      throw new Error("Invalid action type");
-  }
-};
-
-const initialState = {
-  number1: 0,
-  number2: 0,
-  result: 0,
-};
+import { reducer, initialState } from "../reducers/reducerCalculator";
 
 const CalculatorUseReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
